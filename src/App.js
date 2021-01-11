f246ee95a510da4c0c25b8e720230757c7a9fafa
rubenvecg <rubenvec95@gmail.com>
2021-01-11 15:57:48 -0500

    1/11/2021

--- src/App.js
+++ src/App.js
@@ -13,10 +13,26 @@ function App() {
                       <h1>Ruben Vecino</h1>
                       <h3>Aspiring Software Engineer</h3>
 
-                      <div className='links'>
+                      <div id='personal-links' className='links'>
                             <IconGroup types='linkedin github' size='30' spacing='20'></IconGroup>                
                       </div>
                 </header>                
+          </div>
+
+          <div id='about'>
+                <h1>About me</h1>
+                <p>Lorem Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nulla nulla, 
+                      viverra in sapien non, congue convallis purus. Donec non neque diam. Vestibulum 
+                      euismod tempor odio, eget vestibulum est auctor tincidunt. Duis volutpat in leo 
+                      vitae condimentum. Vivamus aliquet lacus eget arcu vulputate, vitae egestas est 
+                      tristique. Suspendisse vehicula, libero eget cursus pretium, magna velit egestas 
+                      arcu, sit amet aliquet nulla justo et ipsum. Proin auctor, eros non tempus varius, 
+                      ante erat efficitur risus, vel dignissim turpis nisl at massa. Phasellus scelerisque 
+                      sapien at nisl elementum, id commodo orci congue. Fusce turpis erat, vulputate eget 
+                      sem suscipit, commodo posuere mauris. Donec non accumsan odio, sit amet porta nisl. 
+                      Fusce vitae finibus lectus. Curabitur nec faucibus elit. Praesent sagittis, lorem a 
+                      aliquam fermentum, odio nisi mattis purus, sed convallis lacus ante id justo. Ut sed 
+                      nulla mauris. Fusce egestas consequat lacus, vitae dictum sem feugiat in.</p>
           </div>
 
           <div id='projects'>
@@ -32,18 +48,16 @@ function App() {
                   We wanted to make the site as fun and lively as possible with  different animated elements.
                 </Card> 
 
-                <Card title='CTP Portable Website'imgName='ctp-portable.gif' madeWith='react html css js'
-                     link='https://ctp-portable.netlify.app/' source='https://github.com/rubenvecg/CTPportable-website'>
-                  This was my first front-end development project using React.js. As part of the ctpHomeHacks hackathon,
-                  I collaborated with our team's UX designer to develop this landing page for a task management app we proposed.
-                  We wanted to make the site as fun and lively as possible with  different animated elements.
+                <Card title='HSL-RGB Color Picker'imgName='color-picker.gif' madeWith=' html css js'
+                     link='https://rubenvecg.github.io/color-picker' source='https://github.com/rubenvecg/color-picker'>
+                  This front-end only tool allows to preview, save, edit and remove color references. Sliders adjust their colors 
+                  dynamically so the user gets an idea of how the color will look. 
                 </Card> 
 
-                <Card title='CTP Portable Website'imgName='ctp-portable.gif' madeWith='react html css js'
-                     link='https://ctp-portable.netlify.app/' source='https://github.com/rubenvecg/CTPportable-website'>
-                  This was my first front-end development project using React.js. As part of the ctpHomeHacks hackathon,
-                  I collaborated with our team's UX designer to develop this landing page for a task management app we proposed.
-                  We wanted to make the site as fun and lively as possible with  different animated elements.
+                <Card title='Responsive Gym Website'imgName='gym-site.gif' madeWith='html css js bootstrap'
+                     link='https://rubenvecg.github.io/gym-site' source='https://github.com/rubenvecg/gym-site'>
+                  As the final project for my CST 2309 (Web Development) class on Fall 2019, I developed this responsive site 
+                  using Bootstrap and CSS media queries, as well as native HTML, CSS and Javascript.
                 </Card>         
             
             </Collection> 
